cb2d33dbd12bebdc88260787240af0cc64e42547
Murat <murat.sertay@icloud.com>
2023-02-13 18:13:29 +0000

    Ci Cd

--- api/utils.js
+++ api/utils.js
@@ -1,18 +1,14 @@
+const Sentry = require("@sentry/node");
+const Tracing = require("@sentry/tracing");
+Sentry.init({
+    dsn: "https://7d83e48b47f243e1bf6b33e2ab5cc834@o4504609634385920.ingest.sentry.io/4504609638318080",
 
-switch (process.env.NODE_ENV) {
-    case "test":
-        require("dotenv").config({path: `${process.cwd()}/.env.test.local`});
-        break;
-    case "development":
-        require("dotenv").config({path: `${process.cwd()}/.env.development.local`});
-        break;
-    case "production":
-        require("dotenv").config({path: `${process.cwd()}/.env.production.local`});
-        break;
-    default:
-        require("dotenv").config({path: `${process.cwd()}/.env.development.local`});
-        break
-}
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+});
 
-
-
+require("dotenv").config({
+    path: `${process.cwd()}/.env.${process.env.NODE_ENV}.local`
+});
