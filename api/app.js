f918cf08b10410c952c1f68fae6a033dcc4b6ef9
charlie lambert <char.lam@ntlworld.com>
2023-02-15 21:59:30 +0000

    added multer middleware

--- api/app.js
+++ api/app.js
@@ -3,6 +3,10 @@ const express = require("express");
 const path = require("path");
 const logger = require("morgan");
 
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
+
+
 const postsRouter = require("./routes/posts");
 const tokensRouter = require("./routes/tokens");
 const usersRouter = require("./routes/users");
@@ -15,6 +19,13 @@ app.use(express.json())
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
+
+//function to upload an image to the uploads folder and set the url
+app.post('/upload', upload.single('image'), (req, res) => {
+    console.log(req.file);
+    const imageUrl = `/uploads/${req.file.filename}`;
+    res.json({ 'imageUrl': imageUrl });
+});
 
 // middleware function to check for valid tokens
 
